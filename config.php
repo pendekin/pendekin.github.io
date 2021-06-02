9de8b240045832dd89362a5b5a72e1fa57a86ddc
pendekin <80629492+pendekin@users.noreply.github.com>
2021-06-02 21:43:35 +0700

    Hh

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ mysql_connect($host,$user,$pass);
 // memilih database yang akan digunakan
 mysql_select_db($db);
 // variabel web
-$site['root'] 	= "http://localhost/shorten/"; // isi dengan alamat domain kamu, jika didalam localhost maka gunakan http://localhost/nama-folder/.
+$site['root'] 	= "https://pendekin.github.com"; // isi dengan alamat domain kamu, jika didalam localhost maka gunakan http://localhost/nama-folder/.
 $site['judul'] 	= "Pendekin"; // isikan dengan judul situs kamu.
 $site['deskripsi'] = "Pendekan URL kamu disini!"; // isikan dengan deskripsi situs kamu.
 ?>
